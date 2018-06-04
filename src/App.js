c80166b639d9a9272fd50959d8351c6544d49f06
Ncedile <ncedile@yoco.co.za>
2018-06-04 14:53:06 +0200

    Added a functional component with some props, name and surname.

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const Greet = ({name, surname}) => (
+  <div>
+    Hello  {name} {surname}, it's great to know you my boy! 
+  </div>
+);
 class App extends Component {
   render() {
+    const dudeToGreet = {name: 'Ncedile', surname: 'Nkonyana'};
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Playing with reactJS!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Greet {...dudeToGreet} />
         </p>
       </div>
     );
